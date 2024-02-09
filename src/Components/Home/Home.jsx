20071f5413bac7d7e93084bbe3735f35cced2d5b
Tirth <tirth.k27@gmai.com>
2024-02-09 17:53:12 +0530

    Final

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './home.css';
+import ImageModal from '../ImageModal';
+
 import video from '../../assets/video.mp4';
 import img1 from '../../assets/1.jpg';
 import img2 from '../../assets/2.jpg';
@@ -11,70 +13,54 @@ import img7 from '../../assets/7.jpg';
 import img10 from '../../assets/img10.jpg';
 
 const Home = () => {
-let data=[
-  {
-    id:1,
-    imgSrc:img1,
-    desc:"Setting up of equipment",
-  },
-  {
-    id:2,
-    imgSrc:img2,
-    desc:"Setting up of equipment",
-  },
-  {
-    id:3,
-    imgSrc:img3,
-    desc:"Setting up of equipment",
-  },{
-    id:4,
-    imgSrc:img4,
-    desc:"Setting up of equipment",
-  },
-  {
-    id:5,
-    imgSrc:img5,
-    desc:"Setting up of equipment",
-  },
-  {
-    id:6,
-    imgSrc:img6,
-    desc:"Setting up of equipment",
-  },
-  {
-    id:7,
-    imgSrc:img7,
-    desc:"Setting up of equipment",
-  },
-  {
-    id:10,
-    imgSrc:img10,
-    desc:"Setting up of equipment",
-  }
-  
-]
+  const [modalData, setModalData] = useState(null);
+
+  let data = [
+    { id: 1, imgSrc: img1, desc: 'Setting up of equipment' },
+    { id: 2, imgSrc: img2, desc: 'Setting up of equipment' },
+    { id: 3, imgSrc: img3, desc: 'Setting up of equipment' },
+    { id: 4, imgSrc: img4, desc: 'Setting up of equipment' },
+    { id: 5, imgSrc: img5, desc: 'Setting up of equipment' },
+    { id: 6, imgSrc: img6, desc: 'Setting up of equipment' },
+    { id: 7, imgSrc: img7, desc: 'Setting up of equipment' },
+    { id: 10, imgSrc: img10, desc: 'Setting up of equipment' },
+  ];
+
+  const handleImageClick = (imgSrc, desc) => {
+    setModalData({ imgSrc, desc });
+  };
+
+  const closeModal = () => {
+    setModalData(null);
+  };
 
   return (
     <>
-    <section className='home'>
-        <video src={video} muted autoPlay loop type='video/mp4'></video>
-        <div className='homeContent container' style={{marginLeft:"10%"}}>
-            <h1>Welcome to our</h1>
-            <h1> Image Gallery</h1>
+      <section className="home">
+        
+        <video src={video} muted autoPlay loop type="video/mp4"></video>
+        
+        <div className="homeContent container" style={{ marginLeft: '10%' }}>
+          <h1>Welcome to our</h1>
+          <h1> Image Gallery</h1>
         </div>
-    </section>
-    <div className="imgs">
+      </section>
+      <div className="imgs">
         <div className="images">
           {data.map((item, index) => (
-          <div className="pics" key={index}>
-            <img src={item.imgSrc} alt="gallery"/>  
-          </div>
+            <div className="pics" key={index} onClick={() => handleImageClick(item.imgSrc, item.desc)}>
+              <img src={item.imgSrc} alt="gallery" />
+              <p className="description">{item.desc}</p>
+            </div>
           ))}
         </div>
-        </div>
-        
-    </>
-  )
-}
+      </div>
 
-export default Home
+      {modalData && (
+        <ImageModal selectedImage={modalData.imgSrc} description={modalData.desc} onClose={closeModal} />
+      )}
+    </>
+  );
+};
+
+export default Home;
